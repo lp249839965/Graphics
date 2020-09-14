901310430f186f5ec8363fcbb8b0a2c8e36dc8a7
Sebastien Lagarde <sebastien@unity3d.com>
2020-09-15 00:47:47 +0200

    Revert "Fixed a collision in the interaction of RTR and RTGI. (#1845)"
    This reverts commit 20451a4bf751ae79f1cba9ffbcb9a01dbebd0099.

--- com.unity.render-pipelines.high-definition/Runtime/Material/BuiltinGIUtilities.hlsl
+++ com.unity.render-pipelines.high-definition/Runtime/Material/BuiltinGIUtilities.hlsl
@@ -109,8 +109,7 @@ void SampleBakedGI(
     // The check need to be here to work with both regular shader and shader graph
     // Note: with Probe volume it will prevent to add the UNINITIALIZED_GI tag and
     // the ProbeVolume will not be evaluate in the lightloop which is the desired behavior
-    // Also this code only needs to be executed in the rasterization pipeline, otherwise it will lead to udnefined behaviors in ray tracing
-#if !defined(_SURFACE_TYPE_TRANSPARENT) && (SHADERPASS != SHADERPASS_RAYTRACING_INDIRECT) && (SHADERPASS != SHADERPASS_RAYTRACING_GBUFFER)
+#if !defined(_SURFACE_TYPE_TRANSPARENT)
     if (_IndirectDiffuseMode == INDIRECTDIFFUSEMODE_RAYTRACE)
         return ;
 #endif
